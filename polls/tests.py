9dd674f3316b796a68fbe346e2f4419a5d6499b9
xaavii0711 <xaavii0711@gmail.com>
2023-02-15 18:31:51 +0100

    exercici 16

--- polls/tests.py
+++ polls/tests.py
@@ -23,6 +23,83 @@ class MySeleniumTests(StaticLiveServerTestCase):
         #cls.selenium.quit()
         super().tearDownClass()
     
+    # def test_login(self):
+    #     self.selenium.get('%s%s' % (self.live_server_url, '/admin/login/'))
+    #     username_input = self.selenium.find_element(By.NAME,"username")
+    #     username_input.send_keys('xmartinez')
+    #     password_input = self.selenium.find_element(By.NAME,"password")
+    #     password_input.send_keys('Superlocal123')
+    #     self.selenium.find_element(By.XPATH,'//input[@value="Log in"]').click()
+
+    #     # self.selenium.find_element(By.XPATH,'//a[@href="/admin/auth/user/]').click()
+
+    #     self.selenium.find_element(By.XPATH,'//a[text()="Users"]').click()
+    #     self.selenium.find_element(By.XPATH,"//a[contains(text(),'Add user')]").click()
+
+    #     username_input2 = self.selenium.find_element(By.NAME,"username")
+    #     username_input2.send_keys('ex9')
+
+    #     password_input2 = self.selenium.find_element(By.NAME,"password1")
+    #     password_input2.send_keys('Superlocal123')
+
+    #     password_input3 = self.selenium.find_element(By.NAME,"password2")
+    #     password_input3.send_keys('Superlocal123')
+
+    #     self.selenium.find_element(By.XPATH,'//input[@value="Save"]').click()
+
+    #     self.selenium.find_element(By.XPATH,'//input[@id="id_is_staff"]').click()
+
+    #     self.selenium.find_element(By.XPATH,'//option[@title="auth | user | Can view user"]').click()
+
+    #     self.selenium.find_element(By.XPATH,'//a[@id="id_user_permissions_add_link"]').click()
+
+    #     self.selenium.find_element(By.XPATH,'//input[@value="Save"]').click()
+
+    #     self.selenium.find_element(By.XPATH,"//a[contains(text(),'Log out')]").click()
+
+    #     self.selenium.find_element(By.XPATH,"//a[contains(text(),'Log in again')]").click()
+
+    #     username_input4 = self.selenium.find_element(By.NAME,"username")
+    #     username_input4.send_keys('ex9')
+
+    #     password_input = self.selenium.find_element(By.NAME,"password")
+    #     password_input.send_keys('Superlocal123')
+
+    #     self.selenium.find_element(By.XPATH,'//input[@value="Log in"]').click()
+
+    #     self.selenium.find_element(By.XPATH,"//a[contains(text(),'Users')]").click()
+    
+    #     try:
+    #         self.selenium.find_element(By.XPATH,"//a[contains(text(),'Add user')]")
+    #         assert False, "Trobat element que NO hi ha de ser"
+    #     except NoSuchElementException:
+    #         pass
+
+    #     self.selenium.find_element(By.XPATH,"//a[contains(text(),'xmartinez')]").click()
+
+    #     try:
+    #         self.selenium.find_element(By.XPATH,"//a[text()='Delete']")
+    #         assert False, "Trobat element que NO hi ha de ser"
+    #     except NoSuchElementException:
+    #         pass
+
+class MySeleniumTests2(StaticLiveServerTestCase):
+    fixtures = ['testdb.json',]
+    
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        opts = Options()
+        opts.headless = settings.HEADLESS_TESTS
+        cls.selenium = WebDriver(options=opts)
+        cls.selenium.implicitly_wait(5)
+    
+    @classmethod
+    def tearDownClass(cls):
+        # no sortim el browser per comprovar visualment com ha anat
+        #cls.selenium.quit()
+        super().tearDownClass()
+    
     def test_login(self):
         self.selenium.get('%s%s' % (self.live_server_url, '/admin/login/'))
         username_input = self.selenium.find_element(By.NAME,"username")
@@ -30,14 +107,11 @@ class MySeleniumTests(StaticLiveServerTestCase):
         password_input = self.selenium.find_element(By.NAME,"password")
         password_input.send_keys('Superlocal123')
         self.selenium.find_element(By.XPATH,'//input[@value="Log in"]').click()
-
-        # self.selenium.find_element(By.XPATH,'//a[@href="/admin/auth/user/]').click()
-
-        self.selenium.find_element(By.XPATH,'//a[text()="Users"]').click()
+        self.selenium.find_element(By.XPATH,'//a[@href="/admin/auth/user/"]').click()
         self.selenium.find_element(By.XPATH,"//a[contains(text(),'Add user')]").click()
 
         username_input2 = self.selenium.find_element(By.NAME,"username")
-        username_input2.send_keys('ex9')
+        username_input2.send_keys('ex16')
 
         password_input2 = self.selenium.find_element(By.NAME,"password1")
         password_input2.send_keys('Superlocal123')
@@ -55,30 +129,72 @@ class MySeleniumTests(StaticLiveServerTestCase):
 
         self.selenium.find_element(By.XPATH,'//input[@value="Save"]').click()
 
+        self.selenium.find_element(By.XPATH,"//a[contains(text(),'Add user')]").click()
+
+        username_input2 = self.selenium.find_element(By.NAME,"username")
+        username_input2.send_keys('prova1')
+
+        password_input2 = self.selenium.find_element(By.NAME,"password1")
+        password_input2.send_keys('Superlocal123')
+
+        password_input3 = self.selenium.find_element(By.NAME,"password2")
+        password_input3.send_keys('Superlocal123')
+        self.selenium.find_element(By.XPATH,'//input[@value="Save"]').click()
+
+        self.selenium.find_element(By.XPATH,'//input[@id="id_is_superuser"]').click()
+
+        self.selenium.find_element(By.XPATH,'//input[@value="Save"]').click()
+
+        self.selenium.find_element(By.XPATH,"//a[contains(text(),'Add user')]").click()
+
+        username_input2 = self.selenium.find_element(By.NAME,"username")
+        username_input2.send_keys('prova2')
+
+        password_input2 = self.selenium.find_element(By.NAME,"password1")
+        password_input2.send_keys('Superlocal123')
+
+        password_input3 = self.selenium.find_element(By.NAME,"password2")
+        password_input3.send_keys('Superlocal123')
+        self.selenium.find_element(By.XPATH,'//input[@value="Save"]').click()
+
+        self.selenium.find_element(By.XPATH,'//input[@id="id_is_superuser"]').click()
+
+        self.selenium.find_element(By.XPATH,'//input[@value="Save"]').click()
+
+        self.selenium.find_element(By.XPATH,"//a[contains(text(),'Add user')]").click()
+
+        username_input2 = self.selenium.find_element(By.NAME,"username")
+        username_input2.send_keys('prova3')
+
+        password_input2 = self.selenium.find_element(By.NAME,"password1")
+        password_input2.send_keys('Superlocal123')
+
+        password_input3 = self.selenium.find_element(By.NAME,"password2")
+        password_input3.send_keys('Superlocal123')
+        self.selenium.find_element(By.XPATH,'//input[@value="Save"]').click()
+
+        self.selenium.find_element(By.XPATH,'//input[@id="id_is_superuser"]').click()
+
+        self.selenium.find_element(By.XPATH,'//input[@value="Save"]').click()
+
         self.selenium.find_element(By.XPATH,"//a[contains(text(),'Log out')]").click()
 
         self.selenium.find_element(By.XPATH,"//a[contains(text(),'Log in again')]").click()
 
         username_input4 = self.selenium.find_element(By.NAME,"username")
-        username_input4.send_keys('ex9')
+        username_input4.send_keys('ex16')
 
         password_input = self.selenium.find_element(By.NAME,"password")
         password_input.send_keys('Superlocal123')
 
         self.selenium.find_element(By.XPATH,'//input[@value="Log in"]').click()
 
-        self.selenium.find_element(By.XPATH,"//a[contains(text(),'Users')]").click()
-    
-        try:
-            self.selenium.find_element(By.XPATH,"//a[contains(text(),'Add user')]")
-            assert False, "Trobat element que NO hi ha de ser"
-        except NoSuchElementException:
-            pass
+        self.selenium.find_element(By.XPATH,'//a[@href="/admin/auth/user/"]').click()
 
-        self.selenium.find_element(By.XPATH,"//a[contains(text(),'xmartinez')]").click()
+        self.selenium.find_element(By.XPATH,"//a[text()='prova1']").click()
 
         try:
-            self.selenium.find_element(By.XPATH,"//a[text()='Delete']")
+            self.selenium.find_element(By.XPATH,"//input[@value='Save and add another']")
             assert False, "Trobat element que NO hi ha de ser"
         except NoSuchElementException:
             pass
